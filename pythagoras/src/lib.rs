ce78723f2785db0007903e894d8c7d6f67c24b18
PontusLaestadius <pontus.laestadius@gmail.com>
2018-05-21 08:34:32 +0200

    Renames function

--- pythagoras/src/lib.rs
+++ pythagoras/src/lib.rs
@@ -6,10 +6,10 @@ use std::f64;
 /// ```
 /// let a = 3;
 /// let b = 4;
-/// let c = pythagoras::equation(a, b);
+/// let c = pythagoras::theorem(a, b);
 /// assert_eq!(5f64, c);
 /// ```
-pub fn equation<T: Into<f64>>(a: T, b: T) -> f64 {
+pub fn theorem<T: Into<f64>>(a: T, b: T) -> f64 {
     let c: f64 = a.into().powi(2) + b.into().powi(2);
     return c.sqrt();
 }
